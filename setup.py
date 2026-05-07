b7437faea07aca892c2bad51f6086bc77f458b94
MalikAdeel-Hull <M.Anjum3-2024@hull.ac.uk>
2026-05-07 22:07:47 +0100

    update

--- setup.py
+++ setup.py
@@ -1,32 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-with open("requirements.txt") as f:
-    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
-
 setup(
-    name="drift-detection-dissertation",
+    name="drift-detection",
     version="1.0.0",
-    author="Malik Adeel",
-    author_email="malikanjum.adeel@gmail.com",
-    description="Data Drift Detection in Healthcare - MSc Dissertation",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/MalikAdeel-Hull/MSc-Dissertation-Drift-Detection",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'scipy',
     ],
-    python_requires=">=3.8",
-    install_requires=requirements,
+    python_requires='>=3.7',
 )
